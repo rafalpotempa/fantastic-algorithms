5d23e6ec947b5daa840ac06ea6c71c68ec922206
Rafał Potempa <rafal.potem@gmail.com>
2021-09-04 15:28:27 +0200

    Add khwarizimi multiplication and visualization decorators

---
+++ mathematics/visualization/fibonacci_matrices.py
@@ -0,0 +1,4 @@
+def visualize(function):
+	def wrapper(n):
+		print(', '.join([str(function(i)) for i in range(n)]))
+	return wrapper
---
+++ mathematics/visualization/multiplication_khwarizmi.py
@@ -0,0 +1,13 @@
+def visualize(function):
+	def wrapper(result, rows):
+		result, rows = function(result, rows)
+		
+		def ok(x):
+			if x%2 != 0: return "+"
+			return ""
+		
+		for x, y in rows:
+			print(f"{x:6} {y:6} {ok(x)}")
+		print("-" * 15)
+		print(f"{result:13}")
+	return wrapper
